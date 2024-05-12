84f1903a9d3799de5b7be6f6f67141bb812dbc76
inisiadev <ravihidayatinisia@gmail.com>
2024-05-13 05:06:37 +0700

    feat: minor stuffs. Skeletons

---
+++ src/pages/home.tsx
@@ -0,0 +1,7 @@
+import { SearchBar } from "@/components/searchBar"
+
+export const Location = () => {
+    return (
+        <SearchBar prompt='Pokemon' placeholder='Enter a valid pokemon name'/>
+    )
+}
