9a497ac4c1990896371597a89bb3f585a7400a28
inisiadev <ravihidayatinisia@gmail.com>
2024-05-13 05:04:30 +0700

    chore: remove unused stuffs

--- src/pages/location.tsx
+++
@@ -1,7 +0,0 @@
-import { SearchBar } from "@/components/searchBar"
-
-export const Location = () => {
-    return (
-        <SearchBar prompt='Pokemon' placeholder='Enter a valid pokemon name'/>
-    )
-}
