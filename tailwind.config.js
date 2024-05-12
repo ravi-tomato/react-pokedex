7e58c3287b71ec6b996491c346b478a6b8bb9cf2
inisiadev <ravihidayatinisia@gmail.com>
2024-05-12 10:25:35 +0700

    feat: add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        'Montserrat': ['Monserrat', 'sans-serif'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import svgr from 'vite-plugin-svgr'
+import Unfonts from 'unplugin-fonts/vite'
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), Unfonts({
+    custom: {
+      families: [
+        {
+          name: 'Montserrat',
+          src: './src/assets/fonts/montserrat/*.ttf',
+        },
+      ],
+    },
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
