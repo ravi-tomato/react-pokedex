f367641133ed1deae644e39bd5d3b4f6f5a3be48
inisiadev <ravihidayatinisia@gmail.com>
2024-05-13 05:05:06 +0700

    chore: another cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import './App.css'
 import { Navbar } from './components/navbar'
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Router } from 'react-router-dom';
-import { Pokemon } from './pages/pokemon';
-import { Location } from './pages/location';
-import { Items } from './pages/items';
 import { AppRoutes } from './AppRoutes';
 
 function App() {
