f52f9c603fa89836b8a4392baa37f0d17dcb278f
ravi-tomato <tomato.202404.alifian@gmail.com>
2024-05-13 17:59:42 +0900

    feat: create pokemon card skeleton

---
+++ src/components/pokemonCard.tsx
@@ -0,0 +1,14 @@
+// type PokemonCardProps = {
+//     name: string,
+//     number: number,
+//     imageUrl: string,
+
+// }
+
+export const PokemonCard = () => {
+    return (
+        <div className="flex flex-column">
+
+        </div>
+    )
+}
